5716170e775ae87b451b5f7416a09fd8462803aa
Andrew J. Stone <andrew.j.stone.1@gmail.com>
2011-12-16 01:53:45 -0500

    code cleanup and report errors and timeouts to cube

--- load.js
+++ load.js
@@ -5,32 +5,30 @@ var EventEmitter = require('events').EventEmitter,
     cube = require('cube')
 ;
 
+var Stats = function() {
+    this.start = 0;
+    this.sent = 0;
+    this.received = 0;
+    this.total_latency = 0;
+    this.errors = 0;
+    this.timeouts = 0;
+};
+
 var Load = module.exports = function(options) {
-    this.stats = {
-        start_time: 0,
-        one_sec_start: 0,
-        one_sec_sent: 0,
-        one_sec_received: 0,
-        one_sec_total_latency: 0,
-        sent: 0,
-        received: 0,
-        in_progress: 0,
-        timeouts: 0,
-        errors: 0,
-        sessions: 0,
-        max_concurrent_sessions: 0,
-        total_sessions_started: 0,
-        total_sessions_completed: 0,
-        total_sessions_error: 0,
-        queued: 0,
-        max_queued: 0,
-        min_latency: 1000000000000,
-        max_latency: 0,
-        one_min_start: 0,
-        one_min_total_latency: 0,
-        one_min_received: 0,
-        one_min_avg_latency: 0
-    };
+    var stats = this.stats = new Stats();
+    stats.sec = new Stats();
+    stats.min = new Stats();
+    stats.in_progress = 0;
+    stats.sessions = 0;
+    stats.max_sessions = 0;
+    stats.total_sessions_started = 0;
+    stats.total_sessions_completed = 0;
+    stats.total_sessions_error = 0;
+    stats.max_queued = 0;
+    stats.queued = 0;
+    stats.min_latency = 1000000000000;
+    stats.max_latency = 0;
+
     this.buckets = [];
     this.tid = null;
     options.time = options.time*1000;
@@ -45,16 +43,15 @@ util.inherits(Load, EventEmitter);
 
 Load.prototype.run = function(callback) {
     var options = this.options;
-    var stats = this.stats;
     var self = this;
     this.http.config = options;
-
     var time = Date.now();
-    stats.start_time = time;
-    stats.one_sec_start = time;
-    stats.one_min_start = time;
-
-    this.tid = setInterval(function() {self.loop(callback);}, options.interval);
+    self.stats.start = time;
+    self.stats.sec.start = time;
+    self.stats.min.start = time;
+    this.tid = setInterval(function() {
+        self.loop(callback);
+    }, options.interval);
 };
 
 Load.prototype.init_buckets = function() {
@@ -77,9 +74,9 @@ Load.prototype.loop = function(callback) {
     var stats = this.stats;
     var now = Date.now();
     this.reset_counters(now);
-    if ((now - stats.start_time) >= options.time) {
-        stats.end_time = now;
-        stats.duration = (stats.end_time - stats.start_time)/1000;
+    if (((now - stats.start) >= options.time) && (stats.start != 0)) {
+        stats.end = now;
+        stats.duration = (stats.end - stats.start)/1000;
         stats.avg_send_rate = stats.sent/stats.duration;
         stats.avg_receive_rate = stats.received/stats.duration;
         clearInterval(this.tid);
@@ -93,15 +90,14 @@ Load.prototype.start_session = function() {
     var stats = this.stats;
     var buckets = this.buckets;
     options.agent = false;
-
     if (!stats.queued && stats.sessions < options.max_sessions &&
-        stats.one_sec_sent <= options.rate) {
+        stats.sec.sent <= options.rate) {
             var rand = Math.floor(Math.random()*buckets.length);
             var index = buckets[rand];
             var session = options.sessions[index];
             stats.sessions++;
-            if (stats.sessions > stats.max_concurrent_sessions) {
-                stats.max_concurrent_sessions = stats.sessions;
+            if (stats.sessions > stats.max_sessions) {
+                stats.max_sessions = stats.sessions;
             }
             stats.total_sessions_started++;
             session.start(function(err) {
@@ -109,34 +105,43 @@ Load.prototype.start_session = function() {
                 if (err) {
                     stats.total_sessions_error++;
                     stats.errors++;
+                    stats.sec.errors++;
+                    stats.min.errors++;
                 } else {
                     stats.total_sessions_completed++;
                 }
             });
     }
 };
+
 Load.prototype.reset_counters = function(now) {
     var stats = this.stats;
-    var delta = now - stats.one_sec_start;
+    var delta = now - stats.sec.start;
 
     // reset the one second counters
     if (delta >= 1000) {
         this.emit('sec', stats);
 	this.update_cube(now);
-        stats.one_sec_start = now;
-        stats.one_sec_sent = 0;
-	stats.one_sec_received = 0;
-	stats.one_sec_total_latency = 0;
+        stats.sec.start = now;
+        stats.sec.sent = 0;
+	stats.sec.received = 0;
+	stats.sec.total_latency = 0;
+        stats.sec.errors = 0;
+        stats.sec.timeouts = 0;
     }
 
     // reset the one minute counters
-    var minDelta = now - stats.one_min_start;
+    var minDelta = now - stats.min.start;
     if (minDelta > 60000) {
-        stats.one_min_avg_latency = stats.one_min_total_latency/stats.one_min_received;
+        stats.min.avg_latency = stats.min.total_latency/stats.min.received;
         this.emit('min', stats);
-        stats.one_min_start = now;
-        stats.one_min_total_latency = 0;
-        stats.one_min_received = 0;
+        stats.min.start = now;
+        stats.min.sent = 0;
+        stats.min.received = 0;
+        stats.min.total_latency = 0;
+        stats.min.errors = 0;
+        stats.min.timeouts = 0;
+        stats.min.avg_latency = 0;
     }
 };
 
@@ -147,9 +152,11 @@ Load.prototype.update_cube = function(now) {
 	    type: 'random',
 	    time: now,
 	    data: {
-		sent: stats.one_sec_sent,
-		received: stats.one_sec_received,
-		latency: stats.one_sec_total_latency/stats.one_sec_received
+		sent: stats.sec.sent,
+		received: stats.sec.received,
+		latency: stats.sec.total_latency/stats.sec.received,
+                errors: stats.sec.errors,
+                timeouts: stats.sec.timeouts
 	    }
 	});
     }
